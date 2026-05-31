e930afc09172b3644d4b85ed4f6cb7b6aff5d682
BhalliBhai <bhallibhai69@gmail.com>
2026-06-01 00:31:54 +0500

    chore: remove next-sitemap dependency and configuration

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  // Replace with your site's actual URL
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://gprm.vercel.app',
-  generateRobotsTxt: true, // (optional)
-  // ...other options
-};
